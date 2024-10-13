67768b526d64544fb6740a58a039e95f6cea1068
MJQ <21494299@student.curtin.edu.au>
2024-10-13 22:34:51 +0800

    Finish

--- LocalDBWebApiUsingEF/Migrations/20241013142810_UpdateBankDataModel.Designer.cs
+++ LocalDBWebApiUsingEF/Migrations/20241013142810_UpdateBankDataModel.Designer.cs
@@ -2,6 +2,7 @@
 using DataTierWebServer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataTierWebServer.Migrations
 {
     [DbContext(typeof(DBManager))]
-    partial class DBManagerModelSnapshot : ModelSnapshot
+    [Migration("20241013142810_UpdateBankDataModel")]
+    partial class UpdateBankDataModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.10");
